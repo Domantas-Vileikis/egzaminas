13fff5884eb7c811f2000bfeadeefea509f345bb
unknown <vileikis.domantas@gmail.com>
2021-04-25 12:20:23 +0300

    slide neveikia / sekanti forma neveikia

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// Mobile menu
+
 document.getElementById('mobile-menu-trigger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.getElementById('navigation').classList.toggle('active');
 });
 
-// TODO: paspaudus nuorodą mobile menu, turėtų susiskleisti
-
-
-// Tabs section
 let tabNavigationBlocks = document.querySelectorAll('.tab-section nav ul li');
 let tabContentBlocks = document.querySelectorAll('.tab-section .tab-content');
 
@@ -34,35 +30,24 @@ for (let tab of tabNavigationBlocks) {
 
     document.querySelector('.tab-section .tab-content[data-slide="'+index+'"]').classList.add('active');
     
-    // swiper.slideTo(index - 1);
   })
 }
 
 
-// Reviews swiper
 const swiper = new Swiper('.reviews-swiper', {
     slidesPerView: 1,
     autoHeight: true,
 
     breakpoints: {
-        // when window width is >= 320px
         600: {
           slidesPerView: 2,
         },
-        // when window width is >= 480px
         1000: {
           slidesPerView: 3,
         }
       },
 
-    // autoplay: {
-    //     delay: 5000
-    // },
 
-    // Optional parameters
-    // loop: true,
-  
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true
